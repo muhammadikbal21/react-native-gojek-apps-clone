92a4e80e2947c49f2f4e68361e69bec26fa0aac3
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-06-20 23:39:28 +0700

    bottom navigation design

--- App.js
+++ App.js
@@ -1,112 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from 'react'
+import { Text, StyleSheet, View, Image } from 'react-native'
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'pink'}}>
+          <Text>Clone Gojek App</Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+        <View style={{height: 54, flexDirection: 'row'}}>
+          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Image style={{width: 26, height: 26}} source={require('./icon/home-active.png')} />
+            <Text style={{fontSize: 10, color: '#545454', marginTop: 4, color: '#43AB4A'}}>Home</Text>
+          </View>
+          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Image style={{width: 26, height: 26}} source={require('./icon/order.png')} />
+            <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>Orders</Text>
+          </View>
+          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Image style={{width: 26, height: 26}} source={require('./icon/help.png')} />
+            <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>Help</Text>
+          </View>
+          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Image style={{width: 26, height: 26}} source={require('./icon/inbox.png')} />
+            <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>Inbox</Text>
+          </View>
+          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Image style={{width: 26, height: 26}} source={require('./icon/account.png')} />
+            <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>Account</Text>
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+const styles = StyleSheet.create({})
