e06b2d849ad7e4853155109494d2bfb67f5298ae
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-06-22 11:04:48 +0700

    add logo white gojek with gradation opacity on image and add button read

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View, Image, TextInput, ScrollView } from 'react-native'
+import { Text, StyleSheet, View, Image, TextInput, ScrollView, TouchableOpacity } from 'react-native'
 
 export default class App extends Component {
   render() {
@@ -100,17 +100,24 @@ export default class App extends Component {
           <View style={{height: 17, backgroundColor: '#F2F2F4', marginTop: 20}} />
           {/* news section */}
           <View style={{paddingTop: 16, paddingHorizontal: 16}}>
-            <View>
+            <View style={{position: 'relative'}}>
               <Image source={require('./dummy/sepak-bola.jpg')} style={{height: 170, width: '100%', borderRadius: 6}} />
+              <View style={{width: '100%', height: '100%', position: 'absolute', top: 0, left: 0, backgroundColor: 'black', opacity: 0.2, borderRadius: 6}} />
+              <View style={{height: 15, width: 60, position: 'absolute', top: 16, left: 16}}>
+                <Image source={require('./logo/white.png')} style={{width: undefined, height: undefined, resizeMode: 'contain', flex: 1}} />
+              </View>
             </View>
-            <View style={{paddingTop: 16, paddingBottom: 20}}>
+            <View style={{paddingTop: 16, paddingBottom: 20, borderBottomColor: '#E8E9ED', borderBottomWidth: 1, marginBottom: 20}}>
               <Text style={{fontSize: 16, fontWeight: 'bold', color: '#1C1C1C'}}>GO-NEWS</Text>
-              <Text style={{fontSize: 14, fontWeight: 'normal', color: '#7A7A7A'}}>Dimas Drajad selamatkan penalti, Timnas U-23 kalahkan Brunnei Darussalam</Text>
+              <Text style={{fontSize: 14, fontWeight: 'normal', color: '#7A7A7A', marginBottom: 11}}>Dimas Drajad selamatkan penalti, Timnas U-23 kalahkan Brunnei Darussalam</Text>
+              <TouchableOpacity style={{backgroundColor: '#61A756', paddingHorizontal: 12, paddingVertical: 11, alignSelf: 'flex-end', borderRadius: 4}} >
+                <Text style={{fontSize: 13, fontWeight: 'bold', color: 'white', textAlign: 'center'}}>READ</Text>
+              </TouchableOpacity>
             </View>
           </View>
         </ScrollView>
         {/* bottom navigation */}
-        <View style={{height: 54, flexDirection: 'row', backgroundColor: 'white'}}>
+        <View style={{height: 54, flexDirection: 'row', backgroundColor: 'white', borderTopWidth: 1, borderTopColor: '#E8E8E8' }}>
           <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Image style={{width: 26, height: 26}} source={require('./icon/home-active.png')} />
             <Text style={{fontSize: 10, color: '#545454', marginTop: 4, color: '#43AB4A'}}>Home</Text>
