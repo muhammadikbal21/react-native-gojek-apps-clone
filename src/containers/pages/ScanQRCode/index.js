81ad051a9b79a602a69bf62f9773cdcbb6c393c9
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-06-27 10:50:34 +0700

    second styling ScanQRCode component

--- src/containers/pages/ScanQRCode/index.js
+++ src/containers/pages/ScanQRCode/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-const IconWithText = () => {
+const IconWithText = (props) => {
   return (
     <View>
-      <View />
-      <Text>Phone Number</Text>
+      <View style={{width: 60, height: 60, backgroundColor: 'green', borderRadius: 70}} />
+      <Text style={{maxWidth: 70, textAlign: 'center', marginTop: 10}}>{props.title}</Text>
     </View>
   )
 }
@@ -30,15 +30,19 @@ const ScanQRCode = () => {
           </View>
         </View>
       </View>
-      <View style={{height: 150, backgroundColor: 'white', paddingHorizontal: 16}}>
+      <View style={{height: 250, backgroundColor: 'white', paddingHorizontal: 16}}>
+        <View style={{alignItems: 'center', marginTop: 8, marginBottom: 18}}>
+          <View style={{width: 40, height: 3, backgroundColor: '#E0E0E0', marginVertical: 1}} />
+          <View style={{width: 40, height: 3, backgroundColor: '#E0E0E0', marginVertical: 1}} />
+        </View>
         <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-          <Text style={{fontSize: 24, fontWeight: 'bold'}}>More Options</Text>
+          <Text style={{fontSize: 24, fontWeight: 'bold', color: 'black'}}>More Options</Text>
           <Text style={{fontSize: 14, color: 'green', fontWeight: 'bold'}}>SHORTCUT</Text>
         </View>
-        <View>
-          <View>
-            <IconWithText />
-            <IconWithText />
+        <View style={{flexDirection: 'row', alignItems: 'flex-start', marginTop: 14, width: '100%'}}>
+          <View style={{flexDirection: 'row', alignItems: 'flex-start', width: 150, justifyContent: 'space-between'}}>
+            <IconWithText title="Phone Number" />
+            <IconWithText title="GoPay Code" />
           </View>
           <View>
             <Text>Your recent GoPay receivers will show here. No admin feels!</Text>
